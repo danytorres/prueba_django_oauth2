22f02119515dcda96ae2c7ef1136f18a8feeba33
Daniel Torres <danytorresbmx@gmail.com>
2020-11-11 22:53:47 -0600

    primer commit

---
+++ unicorn/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Unicorn 
+from .serializers import UnicornSerializer
+
+class UnicornViewSet(viewsets.ModelViewSet):
+
+    queryset = Unicorn.objects.all()
+    serializer_class = UnicornSerializer
